45f50395fae40b8b08479d1536351130599aa7a2
runningiris <irisrunning@foxmail.com>
2020-02-14 12:12:56 +0800

    typescript: class

---
+++ typescript/class.ts
@@ -0,0 +1,212 @@
+// EMCAScript's private field(supported by typescript)
+class Animal {
+    #name: string;
+    constructor(theName: string) { this.#name = theName; }
+}
+
+new Animal("Cat").#name; // Property '#name' is not accessible outside class 'Animal' because it has a private identifier.
+
+// typescript's private field
+class Animal {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+new Animal("Cat").name; // Error: 'name' is private;
+
+
+// private field not compatible
+// It will work if we remove the limit of private modifier on name
+class Animal {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+class Rhino extends Animal {
+    constructor() { super("Rhino"); }
+}
+
+class Employee {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+let animal = new Animal("Goat");
+let rhino = new Rhino();
+let employee = new Employee("Bob");
+
+animal = rhino;
+animal = employee; // Error: 'Animal' and 'Employee' are not compatible
+
+// protected
+// protected can also be accessed within deriving classes
+class Person {
+    protected name: string;
+    constructor(name: string) { this.name = name; }
+}
+
+class Employee extends Person {
+    private department: string;
+
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+let howard = new Employee("Howard", "Sales");
+console.log(howard.getElevatorPitch());
+console.log(howard.name); // error
+
+// A constructor may also be marked protected. This means that the class cannot be instantiated outside of its containing class, but can be extended.
+class Person {
+    protected name: string;
+    protected constructor(theName: string) { this.name = theName; }
+}
+
+// Employee can extend Person
+class Employee extends Person {
+    private department: string;
+
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+let howard = new Employee("Howard", "Sales");
+let john = new Person("John"); // Error: The 'Person' constructor is protected
+
+// Readonly properties must be initialized at their declaration or in the constructor.
+class Octopus {
+    readonly name: string;
+    readonly numberOfLegs: number = 8;
+    constructor (theName: string) {
+        this.name = theName;
+    }
+}
+let dad = new Octopus("Man with the 8 strong legs");
+dad.name = "Man with the 3-piece suit"; // error! name is readonly.
+
+// accessor
+class Employee {
+    fullName: string;
+}
+
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    console.log(employee.fullName);
+}
+
+// get / set
+const fullNameMaxLength = 10;
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (newName && newName.length > fullNameMaxLength) {
+            throw new Error("fullName has a max length of " + fullNameMaxLength);
+        }
+
+        this._fullName = newName;
+    }
+}
+
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    console.log(employee.fullName);
+}
+
+// Static properties
+class Grid {
+    static origin = {x: 0, y: 0};
+    calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+        let xDist = (point.x - Grid.origin.x);
+        let yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+    constructor (public scale: number) { }
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+
+abstract class Department {
+    constructor(public name: string) {
+
+    }
+    printName(): void {
+        console.log("Department name: " + this.name);
+    }
+    abstract printMeeting(): void;
+}
+
+class AccountingDepartment extends Department {
+    constructor() {
+        super("Accounting and Auditing");
+    }
+    printMeeting(): void {
+        console.log("The Accounting Department meets each Monday at 10am.");
+    }
+    generateReports(): void {
+        console.log("Generating accounting reports...");
+    }
+}
+
+let department: Department;
+department = new Department(); // error;
+department = new AccountingDepartment();
+department.printMeeting();
+department.generateReports(); // error: method doesn't exist on declared a abstract type
+
+// Advanced Techniques
+class Greeter {
+    static standardGreeting = "Hello, there!";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        } else {
+            return Greeter.standardGreeting;
+        }
+    }
+}
+
+let greeter1: Greeter;
+greeter1 = new Greeter();
+console.log(greeter1.greet()); // Hello, there!
+
+let greetMaker: typeof Greeter = Greeter;
+greetMaker.standardGreeting = "Hey there!";
+let greeter2: Greeter = new greetMaker();
+console.log(greeter2.greet()); // Hey there!
+
+// using a class as an interface
+class Point {
+    x: number;
+    y: number;
+}
+
+interface Point3d extends Point {
+    z: number;
+}
+
+let point3d: Point3d = { x: 1, y: 2, z: 3 };
